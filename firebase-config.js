fec6220b98b2252d0c447183323120c43c84fe94
Sejin Kim <87762578+power817@users.noreply.github.com>
2026-04-23 13:24:48 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,30 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>휴가 관리자</title>
-</head>
-<body>
-    <div id="calendar"></div>
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, onSnapshot, setDoc } from "firebase/firestore";
 
-    <script type="module">
-        import { initializeApp } from "firebase/app";
-        import { getFirestore, collection, addDoc, onSnapshot } from "firebase/firestore";
+// Firebase 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyA5_A6xsuNDL8y2ErpNJfDpS_s-JrSVDMg",
+  authDomain: "vacation-manager-a9233.firebaseapp.com",
+  projectId: "vacation-manager-a9233",
+  storageBucket: "vacation-manager-a9233.firebasestorage.app",
+  messagingSenderId: "33242163675",
+  appId: "1:33242163675:web:d21b6d5bdb0027a71a9ffe"
+};
 
-        // 제공해주신 Firebase 설정값
-        const firebaseConfig = {
-            apiKey: "AIzaSy...",
-            authDomain: "vacation-manager-...",
-            projectId: "vacation-manager-...",
-            storageBucket: "...",
-            messagingSenderId: "...",
-            appId: "..."
-        };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-        // 초기화
-        const app = initializeApp(firebaseConfig);
-        const db = getFirestore(app);
+// 전역 상태와 연동하기 위해 window 객체에 필요한 함수를 연결합니다.
+window.syncWithFirebase = function(state, onUpdate) {
+  const docRef = doc(db, "data", "shared_state");
 
-        // 이후 여기에 일정 추가/불러오기 로직 작성
-    </script>
-</body>
-</html>
+  // 1. 데이터베이스에서 내용이 바뀌면 내 화면도 업데이트 (실시간)
+  onSnapshot(docRef, (docSnap) => {
+    if (docSnap.exists()) {
+      const remoteData = docSnap.data();
+      onUpdate(remoteData);
+    } else {
+      // 서버에 데이터가 없으면 현재 로컬 데이터를 서버에 업로드
+      setDoc(docRef, state);
+    }
+  });
+
+  // 2. 내가 데이터를 바꿨을 때 서버에 업로드하는 함수
+  window.saveToFirebase = async function(newState) {
+    await setDoc(docRef, newState);
+  };
+};
